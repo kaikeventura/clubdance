57d3adcd8383404a7d0d8a2ce560cb15360ecd08
kaikeventura <kaike.gnr@hotmail.com>
2021-03-06 16:21:55 -0300

    Iniciado construção de templates para uso no frontend

---
+++ src/main/java/com/kaikeventura/clubdance/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.kaikeventura.clubdance.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView home() {
+        return new ModelAndView("index", HttpStatus.OK);
+    }
+}
