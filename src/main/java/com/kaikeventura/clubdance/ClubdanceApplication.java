405d529f4858f6bd88b53273442e8962c059be95
kaikeventura <kaike.gnr@hotmail.com>
2021-03-06 15:08:10 -0300

    first commit

---
+++ src/main/java/com/kaikeventura/clubdance/ClubdanceApplication.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.clubdance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubdanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubdanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaikeventura/clubdance/ClubdanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.clubdance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubdanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
