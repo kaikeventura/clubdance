cc7e251ac112622ebe79869877c31ca079e8d525
kaikeventura <kaike.gnr@hotmail.com>
2021-03-20 14:23:13 -0300

    Criado template com formulário para cadastro de eventos

--- src/main/java/com/kaikeventura/clubdance/domain/event/infra/dto/EventDTO.java
+++ src/main/java/com/kaikeventura/clubdance/domain/event/infra/dto/EventDTO.java
@@ -3,6 +3,7 @@ package com.kaikeventura.clubdance.domain.event.infra.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -20,10 +21,13 @@ public class EventDTO implements Serializable {
 
     private String place;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime startTime;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime endTime;
 
     private Integer capacity;
--- src/main/java/com/kaikeventura/clubdance/domain/event/infra/http/EventController.java
+++ src/main/java/com/kaikeventura/clubdance/domain/event/infra/http/EventController.java
@@ -1,13 +1,16 @@
 package com.kaikeventura.clubdance.domain.event.infra.http;
 
+import com.kaikeventura.clubdance.domain.event.infra.dto.EventDTO;
 import com.kaikeventura.clubdance.domain.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/event")
@@ -17,9 +20,26 @@ public class EventController {
     private final EventService eventService;
 
     @GetMapping
-    public ModelAndView homeEvents(final ModelMap model) {
-        model.addAttribute("activeEventGroup", eventService.allActiveEvents());
+    public ModelAndView homeEvents() {
+        return new ModelAndView(
+                "pages/event/event-home",
+                Map.of("activeEventGroup", eventService.allActiveEvents()),
+                HttpStatus.OK
+        );
+    }
 
-        return new ModelAndView("pages/event/home-event", HttpStatus.OK);
+    @GetMapping("registration-form")
+    public ModelAndView eventRegistration() {
+        return new ModelAndView(
+                "pages/event/event-registration-form",
+                Map.of("eventDTO", new EventDTO()),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("save")
+    public ModelAndView saveANewEvent(final EventDTO eventDTO) {
+
+        return new ModelAndView("redirect:/event/registration-form", HttpStatus.CREATED);
     }
 }
